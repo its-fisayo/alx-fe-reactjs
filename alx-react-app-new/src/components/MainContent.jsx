5c90b2a8ccacdedd7b518e04c17be993fe7c96a0
David Babatunde <davidbabatundehbc@gmail.com>
2025-11-09 18:49:32 +0100

    Add new files

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main>
+            <p style= {{border: '1px solid black', borderRadius: '15px'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
