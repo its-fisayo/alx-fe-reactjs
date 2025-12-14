6d42542bcae976842f41573489c7c95012e0b168
David Babatunde <davidbabatundehbc@gmail.com>
2025-12-14 20:46:29 +0100

    Implement form validation with formik

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,10 +1,11 @@
-import RegistrationForm from './components/RegistrationForm'
+
+import FormikForm from "./components/FormikForm";
 
 function App() {
   return (
     <div>
-      <h1>User Registration</h1>
-      <RegistrationForm />
+      <h1>User Registration (Formik)</h1>
+      <FormikForm />
     </div>
   );
 }
