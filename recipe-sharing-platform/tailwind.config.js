29498431070c8ef963d9d23c5e5d1d285381dc73
David Babatunde <davidbabatundehbc@gmail.com>
2026-01-05 15:08:22 +0100

    Install tailwind properly

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'media', // automatically follows system dark mode
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
